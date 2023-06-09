6082646f0107b9d4e9bad721ea43e041a3884d0a
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-06-09 21:06:13 +0200

    feat: add layers toggle on legend

--- blr-map/src/components/Guide.tsx
+++ blr-map/src/components/Guide.tsx
@@ -1,9 +1,28 @@
-const Guide = () => {
-  return (
-    <div className="guide">
-      
-    </div>
-  )
+import { Legend } from "./MapComponent";
+
+interface GuideProps {
+  legend: Legend[];
+  updateLayerVisibility: (layerName: string) => void;
 }
 
-export default Guide
+const Guide = ({ legend, updateLayerVisibility }: GuideProps) => {
+  const handleClick = (layerName: string) => {
+    updateLayerVisibility(layerName);
+  };
+
+  return (
+    <div className="guide">
+      <h2>Legend</h2>
+      {legend.map((item: Legend, index: number) => (
+        <div key={index}>
+          <span className="layer-name" onClick={() => handleClick(item.layerName)}>
+            {item.layerName}
+          </span>
+          {/* <img src={item.symbol} alt={item.label} /> */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Guide;
--- blr-map/src/components/MapComponent.tsx
+++ blr-map/src/components/MapComponent.tsx
@@ -1,6 +1,6 @@
 import Search from "./Search";
 import Guide from "./Guide";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { loadModules } from "esri-loader";
 
 enum LayerURLs {
@@ -10,8 +10,31 @@ enum LayerURLs {
   Botanic = "https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Leisure_EU/FeatureServer/0",
 }
 
+export interface Legend {
+  layerName: string;
+  symbol: string;
+  label: string;
+}
+
 const MapComponent = () => {
   const MapElement = useRef(null);
+  const [legendInfo, setLegendInfo] = useState<Legend[]>([]);
+  const [visibleLayers, setVisibleLayers] = useState<string[]>([]);
+
+  const updateLayerVisibility = (layerName: string) => {
+    alert(`before:${visibleLayers}`);
+    const isVisible = visibleLayers.includes(layerName);
+
+    if (isVisible) {
+      setVisibleLayers(prevVisibleLayers =>
+        prevVisibleLayers.filter(layer => layer !== layerName)
+      );
+      alert(`after:${visibleLayers}`);
+    } else {
+      setVisibleLayers(prevVisibleLayers => [...prevVisibleLayers, layerName]);
+      alert(`after:${visibleLayers}`);
+    }
+  };
 
   useEffect(() => {
     const setupMap = async () => {
@@ -20,6 +43,8 @@ const MapComponent = () => {
         "esri/views/MapView",
         "esri/layers/FeatureLayer",
       ]);
+
+      const legendItems = [];
 
       const map = new Map({
         basemap: "osm",
@@ -34,31 +59,68 @@ const MapComponent = () => {
 
       const waterwaysLayer = new FeatureLayer({
         url: LayerURLs.Waterways,
+        visible: visibleLayers.includes("Waterways")
       });
 
       const roadsLayer = new FeatureLayer({
         url: LayerURLs.Roads,
+        visible: visibleLayers.includes("Roads")
       });
 
       const touristLayer = new FeatureLayer({
         url: LayerURLs.Tourist,
+        visible: visibleLayers.includes("Tourist")
       });
 
       const botanicLayer = new FeatureLayer({
         url: LayerURLs.Botanic,
+        visible: visibleLayers.includes("Botanic")
       });
 
       map.addMany([waterwaysLayer, roadsLayer, touristLayer, botanicLayer]);
+
+      const [waterwaysLayerInfo, roadsLayerInfo, touristLayerInfo, botanicLayerInfo] = await Promise.all([
+        waterwaysLayer.load(),
+        roadsLayer.load(),
+        touristLayer.load(),
+        botanicLayer.load()
+      ]);
+
+      legendItems.push({
+        layerName: "Waterways",
+        symbol: waterwaysLayerInfo.renderer.symbol,
+        label: "Waterways"
+      });
+      legendItems.push({
+        layerName: "Roads",
+        symbol: roadsLayerInfo.renderer.symbol,
+        label: "Roads"
+      });
+      legendItems.push({
+        layerName: "Tourist",
+        symbol: touristLayerInfo.renderer.symbol,
+        label: "Tourist"
+      });
+      legendItems.push({
+        layerName: "Botanic",
+        symbol: botanicLayerInfo.renderer.symbol,
+        label: "Botanic"
+      });
+
+      setLegendInfo(legendItems);
+
+      console.log(botanicLayerInfo.renderer.symbol)
+
     };
 
     setupMap();
-  }, []);
+  }, [visibleLayers]);
 
   return (
     <>
       <div>
         <Search />
-        <Guide />
+        <Guide legend={legendInfo} updateLayerVisibility={updateLayerVisibility} />
       </div>
       <div id="viewDiv" className="map" ref={MapElement}></div>
     </>
