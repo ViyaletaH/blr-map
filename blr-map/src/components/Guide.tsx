26ee5573b1a2fadde2ae3fb909fd68f907186f71
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-07-11 20:27:43 +0200

    feat: modify guide component

--- blr-map/src/components/Guide.tsx
+++ blr-map/src/components/Guide.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
 
 const Guide = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
-
   return (
     <div className="guide">
       <img
         className="guide-arrow"
         src="/arrow.png"
         alt="guiding list"
-        onClick={toggleVisibility}
         title="Map guide"
       />
-      {/* <div className={isVisible ?  'esri-component esri-layer-list esri-widget esri-widget--panel' : 'guide-list-hidden'}/> */}
     </div>
   );
 };
